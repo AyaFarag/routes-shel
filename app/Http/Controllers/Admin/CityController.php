ef4aaec5529c67827d0d65cb7067f7a2bd9201ab
Aya <ayasaid94.as@gmail.com>
2018-09-02 11:12:44 +0200

    create model,controller, table, request, factory, seeder and resource for city

---
+++ app/Http/Controllers/Admin/CityController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\City;
+use Illuminate\Http\Request;
+use App\Http\Requests\Admin\CityRequest;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $city = City::all();
+        return view('City.View');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('City.Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CityRequest $request)
+    {
+        City::create($request->all);
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(City $city)
+    {
+        return view('City.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, City $city)
+    {
+        $City->update($request->all());
+        return view('City.View');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(City $city)
+    {
+        $City->destory();
+        return back();
+    }
+}
