0a69078c0e9428ee2057e03b5d2de6578bc2f679
Aya <ayasaid94.as@gmail.com>
2018-09-03 11:01:44 +0200

    edit create city form

--- resources/views/admin/city/form.blade.php
+++ resources/views/admin/city/form.blade.php
@@ -1,14 +1,15 @@
+@include("admin.components.select", [
+  "label" => "Countries",
+  "name" => "country_id",
+  "options" => $countries,
+  "class" => "removable-select-input-select"
+])
+
 @include("admin.components.input", [
+  "label" => "City Name",
   "name" => "name",
   "old"  => isset($city) ? $city -> name : "",
  
-]),
-
-@include("admin.components.select", [
-  "name" => "country_id",
-  "options" => $country,
-  "label" => "Countries",
-  "class" => "removable-select-input-select"
 ])
 
 <div>
