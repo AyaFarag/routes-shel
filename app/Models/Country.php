fdc1073b9f4b37f61c239a6edbd8cc81bc71d5a9
Aya <ayasaid94.as@gmail.com>
2018-09-09 16:22:44 +0200

    add address relation in country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -13,4 +13,8 @@ class Country extends Model
     public function cities() {
         return $this -> hasMany(City::class , 'country_id');
     }
+    
+    public function address() {
+        return $this -> hasMany(Address::class);
+    }
 }
