59e187c498dbbda921632e2af349136fcd0ba495
Aya <ayasaid94.as@gmail.com>
2018-09-09 11:59:44 +0200

    create offer views

---
+++ resources/views/admin/offer/create.blade.php
@@ -0,0 +1,24 @@
+@extends("admin.layout.app")
+
+@section("navbar")
+  <h4>Offers</h4>
+@stop
+
+@section("content")
+  <div class="padded-container">
+    <div class="card">
+      <div class="card-content">
+        @component("admin.components.form", [
+          "method" => "post",
+          "action" => route("admin.offer.store")
+        ])
+          @include("admin.offer.form")
+          @include("admin.components.button", [
+            "icon"  => "add",
+            "label" => "create"
+          ])
+        @endcomponent
+      </div>
+    </div>
+  </div>
+@stop
---
+++ resources/views/admin/offer/edit.blade.php
@@ -0,0 +1,24 @@
+@extends("admin.layout.app")
+
+@section("navbar")
+  <h4>Offers</h4>
+@stop
+
+@section("content")
+  <div class="padded-container">
+    <div class="card">
+      <div class="card-content">
+        @component("admin.components.form", [
+          "method" => "put",
+          "action" => route("admin.offer.update", $offer -> id)
+        ])
+          @include("admin.offer.form")
+          @include("admin.components.button", [
+            "icon"  => "replay",
+            "label" => "update"
+          ])
+        @endcomponent
+      </div>
+    </div>
+  </div>
+@stop
