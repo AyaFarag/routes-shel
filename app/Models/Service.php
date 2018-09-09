b906295680ba552e950b72d98d962d2c6404bcaf
Aya <ayasaid94.as@gmail.com>
2018-09-09 16:21:41 +0200

    create table, model, controller , request, resource, factory and seedes for Service

---
+++ app/Models/Service.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    protected $fillable =[
+        'title','description','parent_id'
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo( Service::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany( Service::class, 'parent_id');
+    }
+}
