f73d68faacc148217d433c799e4bab39d57c67f1
Aya <ayasaid94.as@gmail.com>
2018-09-09 16:21:00 +0200

    create table, model, controller , request, resource, factory and seedes for requestTable

---
+++ app/Models/Request.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    protected $fillable = [
+        'user_id','lat','lang','size','images','address'
+    ];
+
+    protected $casts = [
+        "images" => "array"
+    ];
+
+    public function user() {
+        return $this -> belongsTo(User::class );
+    }
+}
