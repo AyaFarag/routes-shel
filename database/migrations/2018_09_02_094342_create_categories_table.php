dff7b7c5aca8bd137911318818e4e27b51a9ab1d
Aya <ayasaid94.as@gmail.com>
2018-09-11 14:22:22 +0200

    edi migration

--- database/migrations/2018_09_02_094342_create_categories_table.php
+++ database/migrations/2018_09_02_094342_create_categories_table.php
@@ -18,8 +18,6 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name');
             NestedSet::columns($table);
-            $table->integer('parent_id')->unsigned()->nullable();
-            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
